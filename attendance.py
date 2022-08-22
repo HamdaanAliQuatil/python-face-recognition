b91beafc4d1d550f485ad6274424b442cf016cbd
Hamdaan Ali <q.hmd08@gmail.com>
2022-08-22 19:31:06 +0530

    feat: add directory and assets for attendance project

--- attendance.py
+++ attendance.py
@@ -5,3 +5,14 @@ import os
 from datetime import datetime
 from PIL import ImageGrab
 
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
