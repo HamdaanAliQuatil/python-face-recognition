d777b3373ac65fa01108f44f5f6589c6542b60bc
Hamdaan Ali <q.hmd08@gmail.com>
2022-08-22 19:50:47 +0530

    feat: add function to mark attendance in a csv file

--- attendance.py
+++ attendance.py
@@ -24,5 +24,15 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
-
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'n{name},{dtString}')
 
